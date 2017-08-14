114897f9d15615328075b301e691272a3d6f94bd
tarepsh <tarepsh@gmail.com>
2017-08-15 02:33:53 +0300

    make sure it's everything is update

--- test.rb
+++ test.rb
@@ -1,10 +1,10 @@
 
 
 #Imprts Sintra gem for webserver and DSL for our Ruby app
-#require 'sinatra'
+require 'sinatra'
 
 # Imports the Google Cloud client library
-#require "google/cloud/vision"
+require "google/cloud/vision"
 
 # Your Google Cloud Platform project ID
 
